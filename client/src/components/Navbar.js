9bb6e8308338978ecd9655ddb0b38c6b37278394
Mike Maynard <mike@pointxero.co>
2024-11-06 21:50:32 +0200

    Intial navivgation and routing

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
 import SpaceDashboardSharpIcon from '@mui/icons-material/SpaceDashboardSharp';
@@ -6,26 +6,57 @@ import LibraryMusicIcon from '@mui/icons-material/LibraryMusic';
 import SpokeIcon from '@mui/icons-material/Spoke';
 import SettingsIcon from '@mui/icons-material/Settings';
 
-
 const Navbar = () => {
-    return (
-      <nav>
-        <ul>
-          <li>
-            <Link to="/"><SpaceDashboardSharpIcon style={{ fontSize: 50 }}/></Link>
-          </li>
-          <li>
-            <Link to="/about"><SpokeIcon style={{ fontSize: 50 }}/></Link>
-          </li>
-          <li>
-            <Link to="/player"><LibraryMusicIcon style={{ fontSize: 50 }}/></Link>
-          </li>
-          <li>
-            <Link to="/settings"><SettingsIcon style={{ fontSize: 50 }}/></Link>
-          </li>
-        </ul>
-      </nav>
-    );
+  // State to track the active link
+  const [activeLink, setActiveLink] = useState('/');  // Default active link is "/"
+
+  // Function to handle link click and update active link
+  const handleLinkClick = (link) => {
+    setActiveLink(link);
   };
 
-export default Navbar
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link
+            to="/"
+            onClick={() => handleLinkClick('/')}
+            className={activeLink === '/' ? 'active' : ''}
+          >
+            <SpaceDashboardSharpIcon style={{ fontSize: 45 }} />
+          </Link>
+        </li>
+        <li>
+          <Link
+            to="/about"
+            onClick={() => handleLinkClick('/about')}
+            className={activeLink === '/about' ? 'active' : ''}
+          >
+            <SpokeIcon style={{ fontSize: 45 }} />
+          </Link>
+        </li>
+        <li>
+          <Link
+            to="/player"
+            onClick={() => handleLinkClick('/player')}
+            className={activeLink === '/player' ? 'active' : ''}
+          >
+            <LibraryMusicIcon style={{ fontSize: 45 }} />
+          </Link>
+        </li>
+        <li>
+          <Link
+            to="/settings"
+            onClick={() => handleLinkClick('/settings')}
+            className={activeLink === '/settings' ? 'active' : ''}
+          >
+            <SettingsIcon style={{ fontSize: 45 }} />
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
